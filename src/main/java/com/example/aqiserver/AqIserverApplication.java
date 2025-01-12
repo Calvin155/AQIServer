8cacf21c20baed072c310e6f49bfe19df04ef8a4
Calvin <calvinlynch181@outlook.com>
2025-01-12 23:14:23 +0000

    ..

---
+++ src/main/java/com/example/aqiserver/AqIserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.aqiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqIserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AqIserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aqiserver/AqIserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aqiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqIserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
